8380f57655bbcf1ddabdf9b711b19e4247088ed2
JeffreyWallace <117045211+JeffreyWallace@users.noreply.github.com>
2023-02-06 17:46:38 -0700

    Changed how the controls work and added a jump

--- GameJam1/Assets/Scripts/PlayerMove.cs
+++ GameJam1/Assets/Scripts/PlayerMove.cs
@@ -7,25 +7,52 @@ public class PlayerMove : MonoBehaviour
 {
     [SerializeField] float spd = 10;
     [SerializeField] float hspd = 5;
+    
+    public Rigidbody rb;
+    public int gamestate=1;
+    public int countdown=180;
+    public int maxSpeed =5;
+    public int jumpheight=5;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb.velocity=new Vector3(0,0,10);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(gamestate==1&&rb.position.y<-1)
+        {gamestate=0;countdown=180;}
+
         float x = Input.GetAxis("Horizontal");
-        transform.Translate(x * hspd * Time.deltaTime, 0, spd * Time.deltaTime); // Move horizontally
-        if(Time.timeScale == 0)
-        {
-            if(Input.GetKeyDown(KeyCode.Space))
+        //transform.Translate(x * hspd * Time.deltaTime, 0, spd * Time.deltaTime); // Move horizontally
+        if(gamestate==1)
+        rb.AddForce(0,0,hspd);
+        if((rb.velocity.x>=maxSpeed&&x<0)||(rb.velocity.x<=-maxSpeed&&x>0)||(rb.velocity.x<maxSpeed&&rb.velocity.x>-maxSpeed))
+        {rb.velocity=new Vector3(x*spd+rb.velocity.x, rb.velocity.y,rb.velocity.z) ;}
+        
+        
+        if(gamestate==0&&countdown<0)//if game is over and timer is finished restart
+        {SceneManager.LoadScene(0);}
+        countdown--;
+        
+            if(Input.GetKeyDown("r"))//restarts tge level
             {
-                SceneManager.LoadScene(0); // Get back to the first scene
-                Time.timeScale = 1;
-                return;
+                SceneManager.LoadScene(0);
             }
-        }
+        
+            if(Input.GetKeyDown(KeyCode.Space))//controlls jumping
+            {
+                rb.velocity=new Vector3(rb.velocity.x,jumpheight,rb.velocity.z);
+            }
+        
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag=="Block")
+        {gamestate=0;countdown=180;}
+
     }
 }
